dc1227b837f8c3cd59da7a17263b48d3bfffb058
Pedro Guilherme <pedropkr3@gmail.com>
2026-05-14 16:01:20 -0300

    [MAIN][OK]

--- src/main/java/br/com/itstime/ItstimeApplication.java
+++ src/main/java/br/com/itstime/ItstimeApplication.java
@@ -1,44 +1,12 @@
 package br.com.itstime;
 
-import br.com.itstime.model.Usuario;
-import br.com.itstime.service.UsuarioService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.Scanner;
 
 @SpringBootApplication
 public class ItstimeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ItstimeApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner demonstracao(UsuarioService usuarioService) {
-		return args -> {
-			Scanner sc = new Scanner(System.in);
-
-			System.out.println("\n=== CADASTRO DE USUARIO ===");
-
-			System.out.print("Nome: ");
-			String nome = sc.nextLine();
-
-			System.out.print("Email: ");
-			String email = sc.nextLine();
-
-			System.out.print("Senha: ");
-			String senha = sc.nextLine();
-
-			try {
-				Usuario u = usuarioService.cadastrar(nome, email, senha);
-				System.out.println("\nUsuario cadastrado! ID: " + u.getId());
-			} catch (RuntimeException e) {
-				System.out.println("\nErro: " + e.getMessage());
-			}
-			sc.close();
-		};
 	}
 }
